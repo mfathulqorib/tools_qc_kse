c6f21580b6251ade59a6729878937a8097aacd05
mfathulqorib <151052562+mfathulqorib@users.noreply.github.com>
2024-01-12 21:26:29 +0700

    Initial commit

---
+++ src/components/providers/index.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+export const Provider = ({ children }) => {
+  return (
+    <NextUIProvider>
+      <div>{children}</div>
+    </NextUIProvider>
+  );
+};
